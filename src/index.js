52d66e0d782ae97d70b8cd3d05a4b89bd2674ef4
Jason Robitaille <robitaille88@gmail.com>
2018-05-30 15:34:27 -0700

    Basic roslib usage example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
+import ROSLIB from 'roslib';
 import App from './App';
 
 const appElement = (<App />);
@@ -8,5 +9,32 @@ const appElement = (<App />);
 if (typeof window !== 'undefined') {
 	render(appElement, document.getElementById('root'));
 }
+
+const ros = new ROSLIB.Ros({
+	url: 'ws://10.194.183.51:9090'
+});
+
+ros.on('connection', function() {
+	console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+	console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+	console.log('Connection to websocket server closed.');
+});
+
+// Subscribing to a Topic
+const listener = new ROSLIB.Topic({
+	ros : ros,
+	name : '/object_classifer/output',
+	messageType : 'object_classifier/classified_object'
+});
+
+listener.subscribe(function(message) {
+	console.log(message);
+});
 
 export default appElement;
